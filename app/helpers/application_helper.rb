f5f2656b5c62e0dc79820a4035c566d20def705a
yapaslhomme <formensah@gmail.com>
2016-02-03 05:57:02 +0100

    newserver

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Afro shopper"
+    base_title = "Dii Gwa"
     if page_title.empty?
       base_title
     else
